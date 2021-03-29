868e9e9939f6b64e6f66615024d181678340a19d
Christian Back <cdirt27@gmail.com>
2021-03-28 23:07:48 -0500

    In music-collection.js: Created function with parameters 'title', 'artist', 'year Published' - Then created object having same propertiesas parameters in function. Pushed new object into collection array, returned console.log(title)

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,6 +1,14 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection (title,artist,yearPublished){
-
+function addToCollection(title, artist, yearPublished) {
+    title = {
+        album: title,
+        creator: artist,
+        birth: yearPublished
+    }
+    collection.push(title);
+    return console.log(title);
 }
+
+addToCollection('Myself', 'Nav', '2017')
